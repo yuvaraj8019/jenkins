b019b30abaf7f8c514ae07ec170deb67cfaf00ba
yuvaraj8019 <yuvaraj8019@gmail.com>
2025-02-19 11:10:08 +0530

    java test 1.0

--- JenkinsTEsting.java
+++ JenkinsTEsting.java
@@ -1,6 +1,7 @@
 public class JenkinsTEsting {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
     }
     
 }
